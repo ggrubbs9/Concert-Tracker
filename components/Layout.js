61303938c6a92c976d699f3ca49d63bf1cec5697
Graham Grubbs <graham.grubbs@avanade3.com>
2021-11-28 16:16:25 -0700

    setup complete

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
 function LayoutComponent({ children }) {
+  const [value, setValue] = useState(0);
+
   return (
     <div>
-      <h1>Hi there!</h1>
       {children}
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
     </div>
   );
 }
